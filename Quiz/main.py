4ebd8b10b3a6a3faeefb6af23ad9c513051c30e1
emobilis <lawrieabuna@gmail.com>
2023-02-14 12:14:32 +0300

    fizzbuzz in range(1,100)

---
+++ Quiz/main.py
@@ -0,0 +1,25 @@
+x = range(1,100)
+for n in x:
+    print(n)
+
+for fizzbuzz in range(1,100):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("fizzbuzz")
+
+    elif fizzbuzz % 3 == 0:
+        print("fizz")
+
+    elif fizzbuzz % 5 == 0:
+        print("buzz")
+
+    print(fizzbuzz)
+
+
+
+
+
+
+
+
+
+
